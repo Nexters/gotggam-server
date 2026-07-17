4a6503b742093b40963530b264b946dab0ca5c70
강예린 <yerin7786@gmail.com>
2026-07-17 20:51:28 +0900

    initial commit

---
+++ src/main/java/com/nexters/death/DeathApplication.java
@@ -0,0 +1,13 @@
+package com.nexters.death;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeathApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeathApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nexters/death/DeathApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nexters.death;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeathApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
