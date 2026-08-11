a45b8120fffdefc39d332b4c52998de30cfa9b9e
gihhyeon <fineblue.kh@gmail.com>
2026-08-11 22:36:46 +0900

    feat: BaseEntity 및 JPA Auditing 설정 추가

---
+++ src/main/java/com/nexters/death/global/entity/BaseCreatedAtEntity.java
@@ -0,0 +1,20 @@
+package com.nexters.death.global.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import java.time.Instant;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseCreatedAtEntity {
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private Instant createdAt;
+
+}
