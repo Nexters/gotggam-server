67cf4b25a3409393289534f9f89457e4429a3cfb
강예린 <yerin7786@gmail.com>
2026-08-13 22:31:30 +0900

    [chore] application yml profiles 환경별 세팅 (#29)
    * chore: local/dev/prod application.yml 프로필 분리
    
    * feat: Swagger 서버 URL을 프로필 설정값으로 노출
    
    * chore: dev 배포 시 Spring 프로필 활성화 및 DB 환경변수명 통일
    
    * chore: SPRING_PROFILES_ACTIVE를 docker-compose 환경변수로 분리
    
    * chore: .env.example 삭제

--- src/main/java/com/nexters/death/global/config/SwaggerConfig.java
+++ src/main/java/com/nexters/death/global/config/SwaggerConfig.java
@@ -2,11 +2,21 @@ package com.nexters.death.global.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import java.util.List;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@EnableConfigurationProperties(SwaggerProperties.class)
 public class SwaggerConfig {
+
+    private final SwaggerProperties swaggerProperties;
+
+    public SwaggerConfig(SwaggerProperties swaggerProperties) {
+        this.swaggerProperties = swaggerProperties;
+    }
 
     @Bean
     public OpenAPI openAPI() {
@@ -15,7 +25,13 @@ public class SwaggerConfig {
             .description("곧감 서비스 API 명세서")
             .version("v0.0.1");
 
-        return new OpenAPI()
+        OpenAPI openAPI = new OpenAPI()
             .info(info);
+
+        if (swaggerProperties.serverUrl() != null) {
+            openAPI.servers(List.of(new Server().url(swaggerProperties.serverUrl())));
+        }
+
+        return openAPI;
     }
 }
